5769013d4fcbf0ef3330cc9aabe785811a997e3f
NehaChoudhary0528 <nehachoudhary0528@gmail.com>
2017-02-07 09:26:55 +1300

    Quiz Solution

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            for (int i = 0; i < 10; i++)
+            {
+                n = 21 * (i*10+9);
+                if (((n%5==4)&&(n%6==3)&&(n%8==1)&&(n%9==0)))
+                {
+                    Console.WriteLine("no. of eggs:{0}",n);
+                }
+            }
+            Console.ReadKey();
+          
+        }
+
+    }
+}
